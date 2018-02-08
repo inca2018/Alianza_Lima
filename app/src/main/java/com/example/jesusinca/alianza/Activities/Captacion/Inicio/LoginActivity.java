c9e946914d9a55dfb03c5ecab1f234bb8a2f03fe
Jesus Inca <jic_d12@hotmail.com>
2018-02-08 17:10:35 -0500

    cambio jesus 06

---
+++ app/src/main/java/com/example/jesusinca/alianza/Activities/Captacion/Inicio/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.jesusinca.alianza.Activities.Captacion.Inicio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jesusinca.alianza.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
